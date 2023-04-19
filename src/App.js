8acae9b4b201ba32f375965bdc9fc6e69927184c
Dmytro Klymchuk <dima.klimchuk.2016@gmail.com>
2023-04-19 16:33:15 +0300

    Work with props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Expenses from './components/Expenses';
-function App() {
+import Expenses from './components/Expenses/Expenses';
+const App = () => {
   const expenses = [
     {
       id: 'e1',
@@ -27,6 +27,6 @@ function App() {
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
